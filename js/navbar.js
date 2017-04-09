af548449739efdecab48c1760d356012d4462c04
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2017-04-09 00:06:12 -0500

    Adding the notification's style

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,7 @@
 let navbar = '#main-navbar';
 let toggles = $(navbar + ' [id^=\'toggle-\']');
 let notificators = $(navbar + ' .main-navbar-notificator');
+let boxNotificators = $(navbar + ' .main-navbar-boxNotification');
 let findOptions = $(navbar + ' #toggle-findOptions');
 let activeElement = null;
 
@@ -20,6 +21,18 @@ let toggleTarget = function(element) {
 	activeElement.addClass('is-hover');
 	$(elementTarget).show();
 }
+
+notificators.each(function(){
+	let target = $(this).attr('data-target');
+	let counterNTF = $(target).children('.ntf').size();
+	if(counterNTF == 0) {
+		$(this).removeClass('activate');
+	} else {
+		$(this).addClass('activate');
+	}
+	$(this).attr('data-counter', counterNTF);
+	console.log(counterNTF);
+});
 
 toggles.on('click', function(){
 	toggleTarget($(this));
