96f9e9d19c4e5dcdfe388925f180dcfcab544e3b
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2017-04-23 12:17:19 -0500

    Adding hash function to the password

--- register.php
+++ register.php
@@ -8,17 +8,17 @@ if (LOGGED_IN)
 }
 
 
-if (isset($_POST['register_username']) && isset($_POST['register_email']) && isset($_POST['register_password']) && isset($_POST['register_password_check']))
+if (isset($_POST['register_username']) && isset($_POST['register_email']) && isset($_POST['register_password']) && isset($_POST['register_password_conf']))
 {
 	$register_username = $_POST['register_username'];
 	$register_email = $_POST['register_email'];
 	$register_password = $_POST['register_password'];
-	$register_password_check = $_POST['register_password_check'];
+	$register_password_conf = $_POST['register_password_conf'];
 
 	$reg_username = filter($register_username);
 	$reg_email = filter($register_email);
 	$reg_password = filter($register_password);
-	$reg_password_check = filter($register_password_check);
+	$reg_password_check = filter($register_password_conf);
 
 	$isValidEmail = $users->IsValidEmail($reg_email) && $register_email == $reg_email;
 	$isValidUser = $users->IsValidName($reg_username) && $register_username == $reg_username;
@@ -26,6 +26,7 @@ if (isset($_POST['register_username']) && isset($_POST['register_email']) && iss
 
 	if (!$users->IsEmailTaken($reg_email) && !$users->IsNameTaken($reg_username) && $reg_password_check == $reg_password)
 	{
+		$reg_password = hash('sha256', $reg_password);
 		dbquery("INSERT INTO estudiantes (usuario, contrasena, correo, nombre, foto, telefono, aboutme) VALUES ('" . $reg_username . "', '" . $reg_password . "', '" . $reg_email . "', '" . $reg_username . "', 'http://i.imgur.com/jFUbUdc.jpg', '', 'Bienvenid@ a Iluminame');");
 		$_SESSION['UBER_USER_E'] = $reg_username;
 		$_SESSION['UBER_USER_H'] = $core->UberHash($reg_password);
