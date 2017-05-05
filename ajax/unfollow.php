1d9f153e94d274c3cbfdba1f6115407b502b3c75
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2017-05-05 09:33:52 -0500

    Adding follow/unfollow notifications

--- ajax/unfollow.php
+++ ajax/unfollow.php
@@ -15,6 +15,8 @@ $id = intval(filter($_POST["id"]));
 dbquery("DELETE FROM estudiantes_seguidores WHERE estudiante_id_seguidor = '" . USER_ID . "' AND estudiante_id_seguido = '" . $id . "';");
 $count = dbqueryEvaluate("SELECT count(*) FROM estudiantes_seguidores WHERE estudiante_id_seguido = '" . $id . "';");
 
+dbquery("INSERT INTO notificaciones (descripcion, fecha, vista, estudiante_id_recibe, estudiante_id_envia) VALUES ('<b>" . filter($myrow["nombre"]) . "</b> ha dejado de seguirte', " . time() . ", '0', " . $id . " , " . USER_ID . ")");
+
 @$myObj->result = "ok";
 $myObj->count = $count;
 $myJSON = json_encode($myObj);
